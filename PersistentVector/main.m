3c48533cdfc6d340343679476524f1336ffaae64
Jon Distad <jon.distad@gmail.com>
2014-01-04 19:13:20 -0500

    Initial Commit

---
+++ PersistentVector/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PersistentVector
+//
+//  Created by Jon Distad on 1/4/14.
+//  Copyright (c) 2014 Jon Distad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
